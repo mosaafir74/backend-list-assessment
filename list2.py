42eb9934f5f1d7b4ca28b70b161ead79b8ef4e6a
haydar mohammad hassan <haydarkassim@yahoo.com>
2019-08-15 02:33:04 -0600

    initial commit

--- list2.py
+++ list2.py
@@ -18,8 +18,11 @@
 
 
 def remove_adjacent(nums):
-    """Your code goes here.  Edit this docstring."""
-    return
+    for x, y in zip(nums, nums[1:]):
+        if x == y:
+            nums.remove(x)
+    return nums
+
 
 
 # E. Given two lists sorted in increasing order, create and return a merged
@@ -27,8 +30,14 @@ def remove_adjacent(nums):
 # The solution should work in "linear" time, making a single pass of both lists.
 # Hint: Don't use `sort` or `sorted` -- they are not linear time.
 def linear_merge(list1, list2):
-    """Your code goes here.  Edit this docstring."""
-    return
+    result = []
+    while list1 and list2:
+        if list1[0] < list2[0]:
+            result.append(list2.pop(0))
+    result.extend(list1)
+    result.extend(list2)
+    return result
+
 
 
 # Simple provided test() function used in main() to print
